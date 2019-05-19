4eb5a8e6eac8b0ab0cff124c2eed4455714c0657
Luke Szanto <luke@livepro.com.au>
2019-05-19 18:41:06 +1000

    Move status code into status package

--- main.go
+++ main.go
@@ -49,13 +49,13 @@ Loop:
 						bucket = time.Now().AddDate(0, 0, 1).Format("02.01.2006")
 					}
 
-					outGoingMessage = getLocationsFromBucket(db, bucket)
+					outGoingMessage = status.GetLocationsFromBucket(db, bucket)
 				} else if strings.Contains(messageText, "tomorrow") {
-					addStatusTomorrow(db, user.Profile.DisplayNameNormalized, strings.TrimSpace(strings.ReplaceAll(strings.TrimSpace(strings.Replace(messageText, botID, "", 1)), "tomorrow", "")))
+					status.AddStatusTomorrow(db, user.Profile.DisplayNameNormalized, strings.TrimSpace(strings.ReplaceAll(strings.TrimSpace(strings.Replace(messageText, botID, "", 1)), "tomorrow", "")))
 
 					outGoingMessage = "Thanks for letting us know <@" + ev.User + ">"
 				} else if strings.Contains(messageText, "today") {
-					addStatusToday(db, user.Profile.DisplayNameNormalized, strings.TrimSpace(strings.ReplaceAll(strings.TrimSpace(strings.Replace(messageText, botID, "", 1)), "today", "")))
+					status.AddStatusToday(db, user.Profile.DisplayNameNormalized, strings.TrimSpace(strings.ReplaceAll(strings.TrimSpace(strings.Replace(messageText, botID, "", 1)), "today", "")))
 
 					outGoingMessage = "Thanks for letting us know <@" + ev.User + ">"
 				} else {
@@ -88,61 +88,4 @@ func getenv(name string) string {
 	}
 
 	return v
-}
-
-func addStatusTomorrow(db *bolt.DB, user string, location string) {
-	bucket := time.Now().AddDate(0, 0, 1).Format("02.01.2006")
-	addStatusToBucket(db, user, location, bucket)
-}
-
-func addStatusToday(db *bolt.DB, user string, location string) {
-	bucket := time.Now().Format("02.01.2006")
-	addStatusToBucket(db, user, location, bucket)
-}
-
-func addStatusToBucket(db *bolt.DB, user string, location string, bucket string) {
-	checkCreateBucket(db, bucket)
-
-	db.Update(func(tx *bolt.Tx) error {
-		b := tx.Bucket([]byte(bucket))
-		err := b.Put([]byte(user), []byte(location))
-
-		return err
-	})
-}
-
-func getLocationsFromBucket(db *bolt.DB, bucket string) string {
-	checkCreateBucket(db, bucket)
-
-	locations := ""
-
-	db.View(func(tx *bolt.Tx) error {
-		// Assume bucket exists and has keys
-		b := tx.Bucket([]byte(bucket))
-
-		c := b.Cursor()
-
-		for k, v := c.First(); k != nil; k, v = c.Next() {
-			locations = locations + string(k) + ": " + string(v) + "\n"
-		}
-
-		return nil
-	})
-
-	if locations == "" {
-		locations = "Sorry! Nobody has let us know their locations for the requested day"
-	}
-
-	return locations
-}
-
-func checkCreateBucket(db *bolt.DB, bucket string) {
-	db.Update(func(tx *bolt.Tx) error {
-		_, err := tx.CreateBucketIfNotExists([]byte(bucket))
-		if err != nil {
-			return fmt.Errorf("create bucket %s", err)
-		}
-
-		return nil
-	})
 }
---
+++ status/status.go
@@ -0,0 +1,68 @@
+package status
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/boltdb/bolt"
+)
+
+// AddStatusTomorrow sets what a user is doing tomorrow and places it in the bucket
+func AddStatusTomorrow(db *bolt.DB, user string, location string) {
+	bucket := time.Now().AddDate(0, 0, 1).Format("02.01.2006")
+	addStatusToBucket(db, user, location, bucket)
+}
+
+// AddStatusToday sets what a user is doing today and places it in the bucket
+func AddStatusToday(db *bolt.DB, user string, location string) {
+	bucket := time.Now().Format("02.01.2006")
+	addStatusToBucket(db, user, location, bucket)
+}
+
+func addStatusToBucket(db *bolt.DB, user string, location string, bucket string) {
+	checkCreateBucket(db, bucket)
+
+	db.Update(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte(bucket))
+		err := b.Put([]byte(user), []byte(location))
+
+		return err
+	})
+}
+
+// GetLocationsFromBucket returns a list of what people are doing from a given bucket
+func GetLocationsFromBucket(db *bolt.DB, bucket string) string {
+	checkCreateBucket(db, bucket)
+
+	locations := ""
+
+	db.View(func(tx *bolt.Tx) error {
+		// Assume bucket exists and has keys
+		b := tx.Bucket([]byte(bucket))
+
+		c := b.Cursor()
+
+		for k, v := c.First(); k != nil; k, v = c.Next() {
+			locations = locations + string(k) + ": " + string(v) + "\n"
+		}
+
+		return nil
+	})
+
+	if locations == "" {
+		locations = "Sorry! Nobody has let us know their locations for the requested day"
+	}
+
+	return locations
+}
+
+func checkCreateBucket(db *bolt.DB, bucket string) {
+	db.Update(func(tx *bolt.Tx) error {
+		_, err := tx.CreateBucketIfNotExists([]byte(bucket))
+		if err != nil {
+			return fmt.Errorf("create bucket %s", err)
+		}
+
+		return nil
+	})
+}
